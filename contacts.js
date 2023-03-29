2dccb40ab44e0f9b50092cf0cda532ed6c5061d5
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-03-29 23:52:23 +0200

    c

---
+++ contacts.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath, "utf-8", (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    console.log(contacts);
+  });
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, "utf-8", (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    console.log(contact);
+  });
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath, "utf-8", (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    const newContacts = contacts.filter((contact) => contact.id !== contactId);
+    fs.writeFile(contactsPath, JSON.stringify(newContacts), (error) => {
+      if (error) throw error;
+      console.log(`contact with id ${contactId} has been removed`);
+    });
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, "utf-8", (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    const newContact = {
+      id: contacts.length + 1,
+      name,
+      email,
+      phone,
+    };
+    const updatedContacts = [...contacts, newContact];
+    fs.writeFile(contactsPath, JSON.stringify(updatedContacts), (error) => {
+      if (error) throw error;
+      console.log(`Contact ${name} has been added`);
+    });
+  });
+}
+
+module.exports = { listContacts, getContactById, addContact, removeContact };
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("hello im learning node.js");
-console.log("i want to see how nodemon works");
+const logger = require("./contacts");
+
+// logger.listContacts();
+// logger.addContact("sarah", "heremail@email.com", "333-444-555");
+// logger.getContactById("e6ywwRe4jcqxXfCZOj_1e");
+// logger.removeContact("rsKkOQUi80UsgVPCcLZZW");
